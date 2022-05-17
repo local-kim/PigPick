e6778abcb593ba0ec77f25058577593121fb17b7
ddanddan <74584001+ddanddan@users.noreply.github.com>
2022-05-17 15:47:30 +0900

    풀당겨주세요

--- SemiProject/src/main/java/project/home/study/SemiProjectApplication.java
+++ SemiProject/src/main/java/project/home/study/SemiProjectApplication.java
@@ -2,8 +2,11 @@ package project.home.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({"project.home.study"})
 public class SemiProjectApplication {
 
 	public static void main(String[] args) {
