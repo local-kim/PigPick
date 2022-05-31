b61736816ccf0219c705e4a5370bebe46dfca07d
김현지 <gumgum28@naver.com>
2022-05-31 11:31:56 +0900

    Update Join Controller : insert birthday in 2 digit

--- SemiProject/src/main/java/data/controller/MemberController.java
+++ SemiProject/src/main/java/data/controller/MemberController.java
@@ -49,6 +49,14 @@ public class MemberController {
 		member.setTel(tel);
 		
 		// 생일
+		if(month.length() == 1) {
+			month = "0" + month;
+		}
+		
+		if(day.length() == 1) {
+			day = "0" + day;
+		}
+		
 		String birthday = month + day;
 		member.setBirthday(birthday);
 		
